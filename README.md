ac5dd7b4c118fc9ef8ba6e0189c84c08ba859c05
Tech Edge <techedge@techedge.su>
2025-01-28 18:29:37 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# The Dude DB Extractor - Node.js
+
+Начиная с версии 6.34, The Dude (серверная часть) переехал с Windows, на Router OS (Mikrotik).
+Формат хранения данных также перетрпел изменения, вместо базы в виде XML файла, отныне всё хранится в SQLite3 DB.
+Грусть и печаль заключается в том, что данные в базе хранятся в бинарном виде, и естественно совершенно нечитаемы.
+
+Данный проект предназначен для декодирования бинарных данных (Устройства и их типы) с переводом оных в читаемый формат.
+
+## Установка
+1. У вас должен быть установлен `Node.js`
+2. Клонируйте репозиторий: `git clone https://github.com/techedge-su/the-dude-db-extractor.git`
+3. Перейдите в директорию проекта, и установите зависимости: `npm install`
+
+## Использование
+1. Отредактируйте файл `src/app.js`, укажите путь до базы данных sqlite3 в константе `DB_PATH`
+2. При необходимости, раскомментируйте строки в объекте `fields`, для добавления их в общий вывод
+3. Запустите скрипт: `npm start`
+
+## Пример результата выполнения
+
+```
+{
+  '21007114': {
+    ip_addresses: [ 'xxx.xxx.xxx.xxx' ],
+    device_type_id: { record_id: 10243, name: 'Имя типа', obj_id: 10243 },
+    record_id: 35594,
+    mac_address: [ 'xx:xx:xx:xx:xx:xx' ],
+    name: 'Имя устройства'
+  }
+}
+```
+Доступные поля для добавления в общий вывод:
+
+```
+name
+ip_addresses
+mac_address
+device_type_id
+record_id
+dns_names
+dns_lookup_interval
+username
+password
+router_os
+parents_id
+custom_field_1
+custom_field_2
+custom_field_3
+snmp_profile_id
+secure_mode
+dude_server
+dns_lookup
+mac_lookup
+polling
+use_notifications
+notifications_id
+agent_id
+probe_interval
+probe_timeout
+probe_down_count
+```
+## Лицензия
+Этот проект распространяется под лицензией MIT.
---
+++ src/app.js
@@ -0,0 +1,154 @@
+const Database = require('better-sqlite3');
+
+DB_PATH='/megathron/js/dude_db/ch.db'
+
+const db = new Database(DB_PATH);
+
+const objsDevices = db.prepare('SELECT * from objs WHERE HEX(obj) LIKE ?').all('4D320100FF8801000F%');
+const objsDeviceTypes = db.prepare('SELECT * from objs WHERE HEX(obj) LIKE ?').all('4D320100FF8801000E%');
+
+const fieldsHead = [ 0x10, 0xfe, 0xff ];
+
+const fields = {
+    '1000fe': 'name',
+    '401f10': 'ip_addresses',
+    '441f10': 'mac_address',
+    '4c1f10': 'device_type_id',
+    '0100fe': 'record_id', // link id ??
+    //'411f10': 'dns_names',
+    //'431f10': 'dns_lookup_interval',
+    //'461f10': 'username',
+    //'471f10': 'password',
+    //'4a1f10': 'router_os',
+    //'571f10': 'parents_id',
+    //'581f10': 'custom_field_1',
+    //'591f10': 'custom_field_2',
+    //'5a1f10': 'custom_field_3',
+    //'4e1f10': 'snmp_profile_id', // 0xffffff is default
+    //'491f10': 'secure_mode',
+    //'4b1f10': 'dude_server',
+    //'421f10': 'dns_lookup',
+    //'451f10': 'mac_lookup',
+    //'511f10': 'polling',
+    //'551f10': 'use_notifications',
+    //'561f10': 'notifications_id',
+    //'4d1f10': 'agent_id', // 0x00 is server
+    //'521f10': 'probe_interval',
+    //'531f10': 'probe_timeout',
+    //'541f10': 'probe_down_count'
+};
+
+const types = [
+    0x21, // Короткая строка
+    0x31, // Массив байтов (MAC адрес)
+    0x08, // Стандартное число (int)
+    0x10, // Большое число (long int)
+    0x18, // Чсло 128bit
+    0x09, // Байт (byte)
+    //0x29, // M2 блок
+    //0xa8, // Массив блоков M2
+    0x88, // Массив чисел
+    0xa0, // Массив строк
+    0x00, // Булево
+    0x01  // Булево
+];
+
+function decode(data, id) {
+
+    const result = {};
+
+    for (let idx = 0; idx < data.length; idx++) {
+        if (fieldsHead.includes(data[idx])) {
+            idx++;
+            const checkField = Array.from(data.slice(idx - 3, idx)).
+                map(byte => byte.toString(16).padStart(2, '0')).join('');
+
+            let type = data[idx]
+            let field = fields[checkField];
+            if (!field) { continue; }
+            result[field] = null;
+            if (!types.includes(type)) { continue; }
+            idx++
+            let size = 0x00;
+            switch (type) {
+                case 0x88:
+                case 0xa0:
+                    result[field] = [];
+                    size = data.readUInt16LE(idx); idx += 2;
+                    for (let aid = 0; aid < size; aid++) {
+                        if (type === 0x88) {
+                            if (field === 'ip_addresses') {
+                                result[field].push(data.slice(idx, idx + 4).map(byte => byte.toString(10)).join('.')); idx += 4;
+                            } else {
+                                result[field].push(data.readUInt32LE(idx)); idx += 4;
+                            }
+                        } else {
+                            const subsize = data.readUInt16LE(idx);  idx += 2;
+                            if (subsize > 0) {
+                                result[field].push(data.slice(idx, idx + subsize).toString('utf8')); idx += subsize;
+                            }
+                        }
+                    }
+                    break;
+                case 0x31:
+                case 0x21:
+                    size = data[idx]; idx++;
+                    if (size > 0) {
+                        if (type === 0x31) {
+                            result[field] = [];
+                            const mac_count = size/6;
+                            for (let mac_num = 0; mac_num < mac_count; mac_num++) {
+                                result[field].push(Array.from(data.slice(idx + (6 * mac_num), (6 + idx) + (6 * mac_num))).map(byte => byte.toString(16).padStart(2, '0')).join(':'));
+                            }
+                        } else {
+                            result[field] = data.slice(idx, idx + size).toString('utf8');
+                        }
+                        idx += size;
+                    }
+                    break;
+                case 0x09:
+                    result[field] =  data[idx];
+                    break;
+                case 0x08:
+                    result[field] = data.readUInt16LE(idx); idx += 4;
+                    break;
+                case 0x10:
+                    result[field] = data.readUInt32LE(idx); idx += 8;
+                    break;
+                case 0x18:
+                    // Скип
+                    idx += 16;
+                    break
+                case 0x00:
+                case 0x01:
+                    result[field] =  type ? true : false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    return result;
+}
+
+const devTypes = {};
+const allDevices = {};
+
+// Обработка типов устройств
+
+for (const obj in objsDeviceTypes){
+    const  pretty = decode(objsDeviceTypes[obj].obj.slice(8), objsDeviceTypes[obj].id);
+    pretty['obj_id'] = objsDeviceTypes[obj].id;
+    devTypes[pretty['record_id']] = pretty;
+
+}
+
+// Обработка устройств
+
+for (const obj in objsDevices){
+    const  pretty = decode(objsDevices[obj].obj.slice(8), objsDevices[obj].id);
+    pretty['device_type_id'] = devTypes[pretty['device_type_id']];
+    allDevices[objsDevices[obj].id] = pretty;
+}
+
+console.log(allDevices);
